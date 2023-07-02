13f7465b16357ad52ea5e3b6a61132184780a9b3
Carol Geng <carolgeng27@gmail.com>
2023-07-02 00:41:44 -0500

    "change title"

--- frontend/Home.js
+++ frontend/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Home({ digitalGoods }) {
   return (
     <div>
-      <h1>Welcome to our Digital Goods Marketplace!</h1>
+      <h1>Welcome to our DigiMart!</h1>
       {digitalGoods.map((good) => (
         <div key={good.id}>
           <h2>{good.name}</h2>
